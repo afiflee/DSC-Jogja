8fc1dcfc859ac2bc26f7637b129bbece43a72537
Erdiawan Anna <erdie.one@gmail.com>
2020-11-17 19:05:50 +0700

    [ADD] GDG Jogjakarta logo

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -1,22 +1,44 @@
 <template>
   <v-container fluid class="mb-0">
     <v-row justify="center" align="center">
-      <v-col>
-        <h1 class="google-font" style="color:#0005DF;">
-          Sponsors
-        </h1>
-        <p class="google-font mb-2">
-          Terima kasih untuk Sponsors 
-          <a
-            href="https://twitter.com/DevFestIndia"
-            rel="noreferrer"
-            target="_blank"
-            style="color:#0005DF;text-decoration:none"
-          >#JogFest2020</a>
-        </p>
-      </v-col>
+        <v-col>
+            <h1 class="google-font" style="color:#0005DF;">
+            Sponsors
+            </h1>
+            <p class="google-font mb-2">
+            Thanks to our Sponsors 
+            <a
+                href="https://twitter.com/DevFestIndia"
+                rel="noreferrer"
+                target="_blank"
+                style="color:#0005DF;text-decoration:none"
+            >#JogFest2020</a>
+            </p>
+        </v-col>
     </v-row>
-    <v-row justify="center" align="center">
+    <v-row>
+        <v-col
+          v-for="align in alignments"
+          :key="align"
+          :align-self="align"
+        >
+        </v-col>
+    </v-row>
+    <v-row no-gutters>
+        <v-col
+            md="4"
+            offset-md="4"
+        >
+            <v-card
+            class="pa-2"
+            outlined
+            tile
+            >
+            .col-md-6 .offset-md-3
+            </v-card>
+        </v-col>
+    </v-row>
+    <v-row justify="center" align="center" class="mt-12">
         <v-col>
             <v-divider></v-divider>
             <br>
@@ -24,23 +46,49 @@
             Community Partners
             </h1>
             <p class="google-font mb-2">
-            Thanks to our community partners
+            Thanks to our Community Partners 
             </p>
         </v-col>
     </v-row>
-    <v-row>
-        <v-col
-            v-for="n in 3"
-            :key="n"
-                >
+    <v-row
+    class="mb-6"
+    justify="center"
+    no-gutters
+    >
+        <v-col cols="12" md="3" sm="3" style="padding: 10px">
+            <img src="@/assets/img/partners/gdg-jogjakarta.svg">
+        </v-col>
+        <v-col cols="12" md="3" sm="3">
             <v-card
-                class="pa-2"
-                outlined
-                tile
+            class="pa-2"
+            outlined
+            tile
             >
-                One of three columns
+            Community Partner 2
+            </v-card>
+        </v-col>
+        <v-col cols="12" md="3" sm="3">
+            <v-card
+            class="pa-2"
+            outlined
+            tile
+            >
+            Community Partner 3
             </v-card>
         </v-col>
     </v-row>
   </v-container>
 </template>
+
+
+<script>
+  export default {
+    data: () => ({
+      alignments: [
+        'start',
+        'center',
+        'end',
+      ],
+    }),
+  }
+</script>
