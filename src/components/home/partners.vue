5c666232bfa4953847e8a837ee2440fa66175089
SokKanaTorajd <t.wijatama.d@gmail.com>
2020-11-21 18:45:02 +0700

    delete height

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -30,7 +30,7 @@
             offset-md="4"
         >
             <a href="https://www.mamikos.com/" target="_blank" rel="noopener noreferrer">
-              <img src="@/assets/img/mamikos.svg" width="375px" height="90px">
+              <img src="@/assets/img/mamikos.svg" width="375px">
               </a>
         </v-col>
     </v-row>
@@ -59,13 +59,13 @@
         <!-- Innovative Academy -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://ia.ugm.ac.id/id/halaman-muka/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/ia-dark-logo.png" width="180px" height="50px">
+                <img src="@/assets/img/partners/ia-dark-logo.png" width="180px">
             </a>
         </v-col>
         <!-- WTM Jogja -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://iamremarkable.withgoogle.com/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_wtm.png" width="250px" height="40px">
+                <img src="@/assets/img/partners/logo_wtm.png" width="250px">
             </a>
         </v-col>
     </v-row>
@@ -74,19 +74,19 @@
         <!-- Permikomnas -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://instagram.com/permikomnas.ri" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_permikomnas.png" width="180px" height="50px">
+                <img src="@/assets/img/partners/logo_permikomnas.png" width="180px">
             </a>
         </v-col>
         <!-- Labs247 -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.labs247.id/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo-labs247.png" width="180px" height="50px">
+                <img src="@/assets/img/partners/logo-labs247.png" width="180px">
             </a>
         </v-col>
         <!-- Digital Inovasi Bangsa -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://dib.biz.id/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/dib.png" width="250px" height="60px">
+                <img src="@/assets/img/partners/dib.png" width="250px">
             </a>
         </v-col>
     </v-row>
@@ -95,13 +95,13 @@
         <!-- Cloud Native Jogja -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.meetup.com/Kubernetes-and-Cloud-Native-Yogyakarta/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/Cloud native-logo.png" width="100px" height="100px">
+                <img src="@/assets/img/partners/Cloud native-logo.png" width="100px">
             </a>
         </v-col>
         <!-- Andronity Solo -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/andronity_solo/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/Andronity.png" width="180px" height="50px">
+                <img src="@/assets/img/partners/Andronity.png" width="180px">
             </a>
         </v-col>
         <!-- Niagahoster -->
@@ -116,19 +116,19 @@
         <!-- UXID Jogja -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://uxid.org/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/UXID jogja.png" width="140px" height="80px">
+                <img src="@/assets/img/partners/UXID jogja.png" width="140px">
             </a>
         </v-col>
         <!-- Block71 Jogja -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://yogyakarta.block71.co/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_block71.png" width="100px" height="60px">
+                <img src="@/assets/img/partners/logo_block71.png" width="100px">
             </a>
         </v-col>
         <!-- Dilo Jogja -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://dilo.id/yogyakarta" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/Dilo.png" width="120px" height="50px">
+                <img src="@/assets/img/partners/Dilo.png" width="120px">
             </a>
         </v-col>
     </v-row>
@@ -137,19 +137,19 @@
         <!-- AMCC -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://amcc.or.id/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_amcc.png" width="200px" height="70px">
+                <img src="@/assets/img/partners/logo_amcc.png" width="200px">
             </a>
         </v-col>
         <!-- Infinite -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://hello.infiniteuny.id/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_infinite.png" width="250px" height="50px">
+                <img src="@/assets/img/partners/logo_infinite.png" width="250px">
             </a>
         </v-col>
         <!-- Danke -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/danke_cc/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/Danke.jpg" width="100px" height="50px">
+                <img src="@/assets/img/partners/Danke.jpg" width="100px">
             </a>
         </v-col>
     </v-row>
@@ -157,13 +157,13 @@
         <!-- HIMASI UAD -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://instagram.com/himasi_uad" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_himasi.png" width="250px" height="100px">
+                <img src="@/assets/img/partners/logo_himasi.png" width="250px">
             </a>
         </v-col>
         <!-- HMSI -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/hmsi_unjaniyk/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_hmsi.png" width="100px" height="100px">
+                <img src="@/assets/img/partners/logo_hmsi.png" width="100px">
             </a>
           </v-col>
     </v-row>
@@ -186,19 +186,19 @@
         <!-- unyku.id -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://unyku.id/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_unykuid.png" width="250px" height="70px">
+                <img src="@/assets/img/partners/logo_unykuid.png" width="250px">
             </a>
         </v-col>
         <!-- teknoevent jogja -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/teknoevent_jogja/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/Logo TeknoEvent Jogja.png" width="100px" height="100px">
+                <img src="@/assets/img/partners/Logo TeknoEvent Jogja.png" width="100px">
             </a>
         </v-col>
         <!-- berita solo raya -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/update_berita_soloraya/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/Update berita.jpg" width="100px" height="100px">
+                <img src="@/assets/img/partners/Update berita.jpg" width="100px">
             </a>
         </v-col>
     </v-row>
@@ -206,13 +206,13 @@
         <!-- teamprojekt -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/teamprojekt_/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/logo_teamprojekt.png" width="190px" height="30px">
+                <img src="@/assets/img/partners/logo_teamprojekt.png" width="190px">
             </a>
         </v-col>
         <!-- teknoevent campus -->
         <v-col cols="12" md="3" sm="3" style="padding: 10px">
             <a href="https://www.instagram.com/teknoeventcampus/" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/img/partners/te_c_blue_solidwhite.png" width="100px" height="50px">
+                <img src="@/assets/img/partners/te_c_blue_solidwhite.png" width="100px">
             </a>
         </v-col>
       </v-row>
