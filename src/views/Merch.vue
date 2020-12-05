a29a660d19d7e82accfa919266475d959f1408c4
Dwi Setiawan <dwisetiawan@linuxmail.org>
2020-12-05 07:37:23 +0700

    fix merch form

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -1,5 +1,5 @@
 <template>
     <v-main>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdvS9Sl57wIfNwF55SKUw8ptS9i4cVHcNVds-E38ziL6p6E2g/viewform?embedded=true" width="640" height="1781" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>    
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdvS9Sl57wIfNwF55SKUw8ptS9i4cVHcNVds-E38ziL6p6E2g/viewform?embedded=true" width="100%" height="1781" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>    
     </v-main>
 </template>
