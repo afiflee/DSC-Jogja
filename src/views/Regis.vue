c53dd91bd08ace2edf3f6b205047db3525ad8c95
afiflee <afif.suwito@gmail.com>
2020-12-27 09:18:26 +0700

    register project

--- src/views/Regis.vue
+++ src/views/Regis.vue
@@ -1,5 +1,5 @@
 <template>
     <v-main>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBYYWbLLbxaUzJVIYx6ltHOvMJfoXEBRdnMXwhPOfD1jFq7g/viewform?embedded=true" width="100%" height="2073" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScQJAYk0zJEL8_qqpK1sEpmkpB1iYbjMKC_ZTnV8mh36J3Yww/viewform?embedded=true" width="100%" height="2503" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
     </v-main>
 </template>
